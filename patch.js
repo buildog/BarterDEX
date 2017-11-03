7ab95018f0f06ed7c75c92f5934fd65bcfdfbb3c
buildog <adrien@buildog.com>
2017-11-03 16:39:50 +0100

    clean

--- patch.js
+++
@@ -1,7 +0,0 @@
-const os = require('os');
-const spawn = require('cross-spawn');
-
-if (os.platform() === 'darwin' || os.platform() === 'linux') {
-    console.log('fix linux permissions')
-    spawn.sync('npm', ['run', 'fixperms']);
-}
