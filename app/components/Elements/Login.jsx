626b646b2a36404ccc31397d0d0f6814cb0c9163
etheodor <etiennetheodore@orange.fr>
2017-11-06 14:01:03 +0000

    remove copy paste

--- app/components/Elements/Login.jsx
+++ app/components/Elements/Login.jsx
@@ -9,36 +9,6 @@ import circles from '../../static/circles.svg';
 import check from '../../static/check.svg';
 import { PassPhraseGenerator } from '../../../config/config';
 
-const electron = require('electron');
-const remote = electron.remote;
-const Menu = remote.Menu;
-
-
-const InputMenu = Menu.buildFromTemplate([{
-        label: 'Undo',
-        role: 'undo',
-    }, {
-        label: 'Redo',
-        role: 'redo',
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Cut',
-        role: 'cut',
-    }, {
-        label: 'Copy',
-        role: 'copy',
-    }, {
-        label: 'Paste',
-        role: 'paste',
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Select all',
-        role: 'selectall',
-    },
-]);
-
 @inject('app')
 @observer
 class Login extends React.Component {
