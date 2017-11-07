99d62357b091430bcc4f5034614de1279c192158
buildog <adrien@buildog.com>
2017-11-07 18:44:50 +0100

    deubg

--- api.js
+++ api.js
@@ -289,7 +289,7 @@ class Emitter extends EventEmitter {
 
 
         const updateBalance = (coinList) => coinList.map((coin) => {
-            if (coin.electrum && !withoutBalance) {
+            if (coin.electrum && !withoutBalance && coin) {
                 return self.listunspent({ coin: coin.coin, address: coin.smartaddress }).then(() => self.balance({ coin: coin.coin, address: coin.smartaddress }).then((coinBalance) => {
                     coin.balance = coinBalance.balance;
                     return coin;
