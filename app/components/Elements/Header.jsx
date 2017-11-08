2f778fc06dd02c5bbf23875f3414776f934936b1
buildog <adrien@buildog.com>
2017-11-08 09:14:16 +0100

    header appversion

--- app/components/Elements/Header.jsx
+++ app/components/Elements/Header.jsx
@@ -25,7 +25,9 @@ class Header extends React.Component {
             <ul>
               <li className="window__title">
                 <i className="window-header-logo" dangerouslySetInnerHTML={{ __html: logo }} />
-                <h1 className="Placeholder-text">Barter<strong>DEX</strong></h1>
+                <h1>Barter<strong>DEX</strong></h1>
+                <small className="window-header-appversion">{ this.props.app.appVersion }</small>
+
               </li>
               <li className="window__controls_right">
                 { userpass.length > 0 && <button className="action danger" onClick={() => this.logout()}>logout</button> }
